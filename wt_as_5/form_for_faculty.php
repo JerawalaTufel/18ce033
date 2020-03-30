c7ee7ec18b6aafb39023e10f8f7a4f714371404e
JerawalaTufel <62898898+JerawalaTufel@users.noreply.github.com>
2020-03-30 22:39:12 +0530

    Add files via upload

---
+++ wt_as_5/form_for_faculty.php
@@ -0,0 +1,61 @@
+<?php
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$cname=$_POST['cname'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$degree=$_POST['degree'];
+$collagep=$_POST['collagep'];
+$gender=$_POST['gender'];
+$phonecode=$_POST['phonecode'];
+$phone=$_POST['phone'];
+$Address=$_POST['Address'];
+if(!empty($fname)||!empty($lname)||!empty($cname)||!empty($email)||!empty($password)||!empty($degree)||!empty($collagep)||!empty($gender)||!empty($phonecode)||!empty($phone)||!empty($Address))
+{
+#code...
+	$host="localhost";
+	$dbusername="root";
+	$dbpassword="";
+	$dbname="tufel";
+	$conn=new mysqli($host,$dbusername,$dbpassword,$dbname)
+if(mysqli_connect_error())
+{
+die('connect error('.mysqli_connect_errorno().')'.mysqli_connect_error());
+}
+else
+{
+	$SELECT="SELECT email from faculty Where email =? Limit 1";
+	$INSERT="INSERT Into faculty (fname,lname,cname,email,password,degree,collagep,gender,phonecode,phone,Address) values (?,?,?,?,?,?,?,?,?,?,?)";
+//prepare statement...
+	$stmt=$conn->prepare($SELECT);
+	$stmt->bind_param("s",$email);
+	$stmt->execute();
+	$stmt->bind_result($email);
+	$stmt->store_result();
+	$rnum=$stmt->num_rows;
+
+	if($rnum==0)
+	{
+     $stmt->close();
+
+     
+$stmt=$conn->prepare($INSERT);
+$stmt->bind_param("ssssssssiis",$fname,$lname,$cname,$email,$password,$degree,$collagep,$gender,$phonecode,$phone,$Address);
+$stmt->execute();
+echo "new record inserted sucessfully";
+	}
+	else
+	{
+		echo "some one alrady register using this email";
+
+	}
+$stmt->close();
+$conn->close();
+}
+}
+else
+{
+echo "All field are required";
+die();
+}
+?>
---
+++ wt_as_5/form_for_students.php
@@ -0,0 +1,59 @@
+<?php
+$fname=$_POST['fname'];
+$lname=$_POST['lname'];
+$cname=$_POST['cname'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+$gender=$_POST['gender'];
+$phonecode=$_POST['phonecode'];
+$phone=$_POST['phone'];
+$Address=$_POST['Address'];
+if(!empty($fname)||!empty($lname)||!empty($cname)||!empty($email)||!empty($password)||!empty($gender)||!empty($phonecode)||!empty($phone)||!empty($Address))
+{
+#code...
+	$host="localhost";
+	$dbusername="root";
+	$dbpassword="";
+	$dbname="tufel";
+	$conn=new mysqli($host,$dbusername,$dbpassword,$dbname)
+if(mysqli_connect_error())
+{
+die('connect error('.mysqli_connect_errorno().')'.mysqli_connect_error());
+}
+else
+{
+	$SELECT="SELECT email from student Where email =? Limit 1";
+	$INSERT="INSERT Into student (fname,lname,cname,email,password,gender,phonecode,phone,Address) values (?,?,?,?,?,?,?,?,?)";
+//prepare statement...
+	$stmt=$conn->prepare($SELECT);
+	$stmt->bind_param("s",$email);
+	$stmt->execute();
+	$stmt->bind_result($email);
+	$stmt->store_result();
+	$rnum=$stmt->num_rows;
+
+	if($rnum==0)
+	{
+     $stmt->close();
+
+     
+$stmt=$conn->prepare($INSERT);
+$stmt->bind_param("ssssssiis",$fname,$lname,$cname,$email,$password,$gender,$phonecode,$phone,$Address);
+$stmt->execute();
+echo "new record inserted sucessfully";
+	}
+	else
+	{
+		echo "some one alrady register using this email";
+
+	}
+$stmt->close();
+$conn->close();
+}
+}
+else
+{
+echo "All field are required";
+die();
+}
+?>
